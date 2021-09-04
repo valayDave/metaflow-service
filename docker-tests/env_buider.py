1716c2c53ecc40a375bca373453ef0654354bfa3
Valay Dave <valaygaurang@gmail.com>
2021-09-03 19:20:40 -0700

    temporary fix.

--- docker-tests/env_buider.py
+++ docker-tests/env_buider.py
@@ -207,7 +207,7 @@ class DockerTestEnvironment:
         
         self._logger('Creating Metadata Service Container',fg='blue')
         # Create the metadata service container
-        self._metadataservice_container = self._docker.containers.run(self._image_name,\
+        self._metadataservice_container = self._docker.containers.run('netflixoss/metaflow_metadata_service',\
                                                     detach=True,\
                                                     environment=self._mdcontainer_env_vars(),\
                                                     network=self._network_name,\
